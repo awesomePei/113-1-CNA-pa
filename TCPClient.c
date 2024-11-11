02cbc2063f2062b0d9a80812a597781c2fd136ba
Pei Tsai <pj@PeideMacBook-Air.local>
2024-11-11 22:38:04 +0800

    can register, log in, list, and exit

--- TCPClient.c
+++ TCPClient.c
@@ -10,10 +10,20 @@
 #define PORT 8888
 #define SA struct sockaddr
 
+void communicate_with_server(int sockfd, const char *message) {
+    printf("Awaiting server response...");
+    char buffer[MAX];
+    write(sockfd, message, strlen(message)); // client sends messages to server
+    bzero(buffer, sizeof(buffer)); // empty out the buffer
+    read(sockfd, buffer, sizeof(buffer)); // client reads response sent by server
+    printf(buffer);
+}
+
 int main() 
 {
     int sockfd, connfd;
     struct sockaddr_in servaddr, cli; // sockaddr_in designed to store IPv4 address info for sockets (IP, port num, address family)
+    char user_input[MAX];
 
     // socket file descriptor 
     // 選擇 address family (AF_INET), type of socket (TCP socket) 
@@ -39,7 +49,28 @@ int main()
         printf("Successfully connected to the server.\n");
     }
 
-    // 
+    // Take messsages
+    while(1) {
+        // get input
+        fgets(user_input, MAX, stdin); 
+
+        // get rid of the new line character
+        user_input[strcspn(user_input, "\n")] = 0; 
+
+        if (strcmp(user_input, "Exit") == 0) {
+            communicate_with_server(sockfd, "Exit");
+            break;
+        } else if (strcmp(user_input, "List") == 0) {
+            communicate_with_server(sockfd, "List");
+        } else if (strncmp(user_input, "REGISTER#", strlen("REGISTER#")) == 0) {
+            communicate_with_server(sockfd, user_input);
+        } else if (strchr(user_input, '#') != NULL) {
+            communicate_with_server(sockfd, user_input);
+        } else {
+            communicate_with_server(sockfd, user_input);
+        }
+
+    }
 
     // Close the client socket
     close(sockfd);
